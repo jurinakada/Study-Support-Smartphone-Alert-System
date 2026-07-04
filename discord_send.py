220fd93321c3f954ed904a3ef8aa9f985189830f
Nakada Juri <157940688+nak-juuri7@users.noreply.github.com>
2026-07-04 23:44:01 +0900

    discord part modified. added the test code.

--- discord_send.py
+++ discord_send.py
@@ -1,3 +1,11 @@
+from discordwebhook import Discord
+discord = Discord(url="webhook url")
 
 class disc_send:
-    dis_send()
+    #test
+    dis_send(final_report):
+        discord.post(content='final_report')
+
+    dis_send("hello this is test")
+
+
