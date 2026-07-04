fee52359d7da01bc8f7974317ff23628349ceb1b
Nakada Juri <157940688+nak-juuri7@users.noreply.github.com>
2026-07-05 01:05:12 +0900

    discord_send.py modified temporarily testing code

--- discord_send.py
+++ discord_send.py
@@ -1,34 +1,69 @@
-from discordwebhook import Discord
+# from discordwebhook import Discord
+# import requests
+# import os
+
+# #discord = Discord(url="webhook url")
+
+# # WEBHOOK_URL = "https://discord.com/api/webhooks/YOUR_WEBHOOK_URL"
+# WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+
+# class disc_send:
+#     #test
+#     # dis_send(final_report):
+#     #     discord.post(content='final_report')
+
+#     # dis_send("hello this is test")
+
+#     def  warning_notification(warning_message):
+#         data = {
+#             #send warning message to discord
+#             "content": "this is test for sending final report"
+#             }
+
+#     def send_final_report(final_report):
+#         data = {
+#         #"content": final_report
+#         #send final report to discord
+#         "content": "this is test for sending final report"
+#         }
+
+#         try:
+#             response = requests.post(WEBHOOK_URL, json=data)
+
+#             if response.status_code == 204:
+#                 print("Message sent successfully!")
+#             else:
+#                 print(f"Failed to send: {response.status_code}")
+
+#         except requests.exceptions.RequestException as e:
+#             print("Network error. Final report was not sent.")
+#             print(e)
+            
+            
+import os
 import requests
+from dotenv import load_dotenv
 
-#discord = Discord(url="webhook url")
+load_dotenv()
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/YOUR_WEBHOOK_URL"
+WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 
 
-class disc_send:
-    #test
-    # dis_send(final_report):
-    #     discord.post(content='final_report')
+def send_discord(message):
+    data = {
+        "content": message
+    }
 
-    # dis_send("hello this is test")
+    response = requests.post(WEBHOOK_URL, json=data)
 
-    warning_notification():
+    if response.status_code == 204:
+        print("Sent successfully!")
+    else:
+        print("Failed:", response.status_code)
 
-    send_final_report(final_report):
-        data = {
-        #"content": final_report
-        "content": "this is test for sending final report"
 
-        }
-
-        try:
-            response = requests.post(WEBHOOK_URL, json=data)
-        
-        if response.status_code == 204:
-            print("Message sent successfully!")
-        else:
-            print(f"Failed to send: {response.status_code}")
+if __name__ == "__main__":
+    send_discord("Hello from Raspberry Pi!")
 
 
 
